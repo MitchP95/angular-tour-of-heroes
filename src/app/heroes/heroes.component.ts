c5c9d3cd6ac0cc72b47e19cd5f993947927f7ddf
Mitch P <mitch.puersten@gmail.com>
2022-11-30 20:08:42 -0500

    Added mock heroes, displayed in heroes component (*ngFor)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,6 +12,8 @@ export class HeroesComponent {
     id: 1,
     name: 'Windstorm'
   }
+
+  heroes = HEROES;
 
   constructor() {}
 

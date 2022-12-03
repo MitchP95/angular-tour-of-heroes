9343e451a855f234e37003cc2f5622f92005a626
Mitch P (mobile) <mitch.puersten@gmail.com>
2022-12-03 14:06:02 -0500

    Added message when hero is selected.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessagesService } from '../messages.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,7 +13,8 @@ export class HeroesComponent {
   selectedHero?: Hero;
 
   constructor(
-    private heroService: HeroService
+    private heroService: HeroService,
+    private messageService: MessagesService
   ) {}
 
   ngOnInit(): void {
@@ -26,5 +28,7 @@ export class HeroesComponent {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(
+      'HeroesComponent: Selected hero id=' + hero.id);
   }
 }

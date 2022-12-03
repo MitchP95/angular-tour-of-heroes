70df5c20c63077d07a5c5c959706a458575b11c0
Mitch P (mobile) <mitch.puersten@gmail.com>
2022-12-03 14:00:05 -0500

    Added message service html and injected for use with heroService.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of} from 'rxjs';
 
 import { Hero } from './hero';
+import { MessagesService } from './messages.service';
 import { HEROES } from './mock-heroes';
 
 @Injectable({
@@ -9,10 +10,13 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(
+    private messageService: MessagesService
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessagesService } from '../messages.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./messages.component.scss']
 })
 export class MessagesComponent {
+  constructor (
+    public messageService: MessagesService
+  ) {}
 
 }
